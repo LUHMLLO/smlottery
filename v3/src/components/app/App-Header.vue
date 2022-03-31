2f6f730e2030e4984c956e0a0b90150b52781501
LUHMLLO <luhmllo06@protonmail.com>
2022-03-31 16:07:07 -0400

    fixing header on mobile

--- v3/src/components/app/App-Header.vue
+++ v3/src/components/app/App-Header.vue
@@ -5,15 +5,15 @@ import { useSidebarStore } from "@/store/sidebar";
 <template>
   <min-header class="px-0 py-48">
     <min-container class="px-24 md:px-0">
-      <min-row content="center" items="center" class="gap-12">
+      <min-grid content="center" items="center" class="grid-2">
         <min-row content="start" items="center" class="gap-12">
           <i class="uil-analytics font-xl color-accent" style="transform: rotate(90deg)" @click="useSidebarStore().on()" />
-          <h4 class="font-lg md:font-xl bold color-tertiary">Dashboard</h4>
+          <h4 class="font-regular sm:font-lg md:font-xl bold color-tertiary">Dashboard</h4>
         </min-row>
         <min-row content="end" items="center" class="gap-12">
           <i class="uil-sunset font-xl color-yellow-500" />
         </min-row>
-      </min-row>
+      </min-grid>
     </min-container>
   </min-header>
 </template>
